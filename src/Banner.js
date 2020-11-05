99b4e3c0660a14d6405737a951a07424f0f98718
Akshat <akshatsaraswat03@gmail.com>
2020-11-05 12:22:20 +0530

    website complete

--- src/Banner.js
+++ src/Banner.js
@@ -1,28 +1,28 @@
 import React, { useState } from 'react'
-import Search from './Search'
 import './Banner.css'
-import { Button } from "@material-ui/core"
+import { Button } from "@material-ui/core";
+import Search from './Search';
+import { useHistory } from "react-router-dom";
 
-
-const Banner = () => {
-  const [showSearch, setShowSearch] = useState(false)
-
-  const onClick = () => {
-    setShowSearch(!showSearch)
-  }
+function Banner() {
+  const history = useHistory();
+  const [showSearch, setShowSearch] = useState(false);
 
   return (
-    <div className="banner">
-      <div className="banner-search">
+    <div className='banner'>
+      <div className='banner__search'>
         {showSearch && <Search />}
 
-
-        <Button variant="outlined" onClick={onClick} className="banner-searchBtn">Search Dates</Button>
+        <Button onClick={() => setShowSearch(!showSearch)} className='banner__searchButton' variant='outlined'>
+          {showSearch ? "Hide" : "Search Dates"}
+        </Button>
       </div>
-      <div className="banner-info">
+      <div className='banner__info'>
         <h1>Get out and stretch your imagination</h1>
-        <h5>Plan a different kind of gateway to uncover the hidden gems near you</h5>
-        <Button variant='outlined'>Explore Nearby</Button>
+        <h5>
+          Plan a different kind of getaway to uncover the hidden gems near you.
+                </h5>
+        <Button onClick={() => history.push('/search')} variant='outlined'>Explore Nearby</Button>
       </div>
     </div>
   )
